87e0c3c52c9ad6da7310967d7f9db91660dc58a6
Vlad Stanescu <vladstanescu94@gmail.com>
2020-12-08 12:05:38 +0200

    Add test to getCityName

--- weatheriosTests/LocationGeocodeTest.swift
+++ weatheriosTests/LocationGeocodeTest.swift
@@ -6,27 +6,20 @@
 //
 
 import XCTest
+import CoreLocation
+@testable import weatherios
 
 class LocationGeocodeTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testReverseGeocodeToCityName() throws {
+        let sut = LocationViewModel()
+        let sibiuLocation = CLLocation(latitude: 45.79781812434426, longitude: 24.14944932344487)
+        
+        sut.getCityName(for: sibiuLocation) { placemark in
+            sut.city = placemark?.first?.locality ?? ""
+            XCTAssertTrue(sut.city == "Sibiu")
         }
     }
+
 
 }
